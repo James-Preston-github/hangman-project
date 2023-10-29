e1975a62f58b35bb530f247398dd4d4daa3fc03a
James <jprpreston@hotmail.co.uk>
2023-10-29 12:33:05 +0000

    Finished the coding, word list is short but that's the only issue, maybe some repeated code with checking the input is valid everywhere

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ An attempt to code the classic game hangman as efficiently as possible.
 To install this fun, little game. Just go into your terminal (assuming you've already got git) and use the code: git clone {the URL of this repo}
 So far, I've made a list of all the letters, and taken in an input that checks if your input is a single letter.
 I've also done some encapsulation to make the code easier to read and any issues with it easier to diagnose, it's still small as of writing but it'll help in the long run
+
+I've made the game, I had to do some work to make sure that the game only accepted one letter inputs and that it wouldn't penalise you for guessing 'Q' and 'q', but now it works perfectly, my word list is small so I'm going to google a list online that contain more letters. Though it was handy for testing to know all the letters.
---
+++ milestone_5.py
@@ -0,0 +1,68 @@
+import random as rd
+from milestone_2 import word_list
+
+def make_word_guessed(word):
+    list_of_blanks=[]
+    list_of_letters=set({})
+    for letter in word:
+        list_of_blanks.append('_')
+        list_of_letters.add(letter)
+    return [list_of_blanks, list_of_letters]
+
+def make_num_letters(word, word_guessed):
+    unique_letters=make_word_guessed(word)[1]
+    num_of_unique_letters=len(unique_letters)
+
+
+
+class Hangman:
+    def __init__(self,word_list,num_lives=5):
+        self.num_lives=num_lives
+        self.word_list=word_list
+        self.word=rd.choice(word_list)
+        self.word_guessed=make_word_guessed(self.word)[0]
+        self.num_letters=len(make_word_guessed(self.word)[1])
+        self.list_of_guesses=[]
+    
+    def check_guess(self,guess):
+        guess=guess.lower().strip()
+        print(guess)
+        if guess in self.word:
+            print(f'Good guess, {guess} is in the word')
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed[i]=guess
+            self.num_letters-=1
+            print(self.word_guessed)
+        else:
+            self.num_lives-=1
+            print(f'Sorry, {guess} isn\'t in the word')
+            print(f'Sorry, you lose a life, you now have {self.num_lives} lives left')
+
+    def ask_for_input(self):
+            guess=input("Guess a letter that might be in the word")
+            if not guess.strip().isalpha() or len(guess.strip())>1:
+                print('Invalid letter, guess again')
+            elif guess.lower() in self.list_of_guesses:
+                print('You already tried that letter')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess.lower())
+
+
+def play_game(word_list):
+    num_lives=5
+    game=Hangman(word_list,num_lives)
+    while True:
+        if num_lives==0:
+            print('You lose')
+            break
+        elif game.num_letters >0:
+            game.ask_for_input()
+        else:
+            print(f'Congrats, you won the game! And you had {game.num_lives} lives left')
+            break
+        
+
+play_game(word_list)
+
